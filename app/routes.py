0d6c1f65737bdf61bfdba2baf25c451c112f3072
anitika2905 <44600529+anitika2905@users.noreply.github.com>
2019-06-27 09:49:05 +0530

    Add files via upload

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+@app.route('/')
+@app.route('/index')
+def index():
+    user={'username':'NITIKA'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - microblog</title>
+     </head>
+      <body bgcolor='cyan'>
+         <h1>hello, '''+user['username']+''' </h1>
+        </body>
+</html>'''
